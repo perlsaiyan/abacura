21b5fc269aafdaa36e7d692418cb8d85eaaa051f
Tom Allen <tom@nonserviam.net>
2023-06-03 14:29:30 -0400

    Fixes to package

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup
 setup(
     name="abacura",
     description="Multi-session MUD client written in Python with Textual library",
-    version="0.0.1",
-    packages=["abacura", "abacura.mud"],
+    python_requires='>3.10',
+    version="0.0.2",
+    packages=["abacura", "abacura.mud", "abacura.mud.options"],
     license="Proprietary",
     classifiers=[
         'License :: Other/Proprietary License',
     ],
+
     include_package_data=True,
+    package_data={
+        'abacura': ['*.css'],
+    },
     install_requires=["textual~=0.27.0","asynctelnet~=0.2.5"],
     entry_points="""
         [console_scripts]
