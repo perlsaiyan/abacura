da2d5ff8541e7f1e39fd1397ff0506a282f66844
Tom Allen <tom@nonserviam.net>
2023-06-17 01:19:18 -0400

    log gossip to the commTL

--- abacura_kallisti/plugins/comms.py
+++ abacura_kallisti/plugins/comms.py
@@ -1,18 +1,16 @@
 """LOK Communications plugins"""
+from __future__ import annotations
+
 from textual import log
 from textual.widgets import TextLog
 
+from abacura.mud import OutputMessage
 from abacura.plugins import Plugin, action
 
 class LOKComms(Plugin):
 
-    def __init__(self):
-        super().__init__()
-
-        #self.session.director.register_object(self.test_gos)
-        
-    @action(r"\x1B\[1;35m<Gossip: (.*)> \'(.*)\'", color=True)
-    def test_gos(self, *args, **kwargs):
+    @action(r"\x1B\[1;35m<Gossip: .*", color=True)
+    def test_gos(self, msg: OutputMessage):
+        """Send gossips to the commslog"""
         commsTL = self.session.screen.query_one("#commsTL", expect_type=TextLog)
-        log(f"found {commsTL} for {args[0]} and {args[1]}")
-        commsTL.write(f"{args[0]}: {args[1]}")
+        commsTL.write(f"{msg.message}")
