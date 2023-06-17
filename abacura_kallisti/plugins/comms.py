2ae9500f657cba421abfbdd830681cbff98497e6
Tom Allen <tom@nonserviam.net>
2023-06-17 01:22:35 -0400

    more efficient comms log

--- abacura_kallisti/plugins/comms.py
+++ abacura_kallisti/plugins/comms.py
@@ -1,5 +1,6 @@
 """LOK Communications plugins"""
 from __future__ import annotations
+from typing import Optional
 
 from textual import log
 from textual.widgets import TextLog
@@ -8,9 +9,11 @@ from abacura.mud import OutputMessage
 from abacura.plugins import Plugin, action
 
 class LOKComms(Plugin):
+    comms_textlog: Optional[TextLog] = None
 
     @action(r"\x1B\[1;35m<Gossip: .*", color=True)
     def test_gos(self, msg: OutputMessage):
         """Send gossips to the commslog"""
-        commsTL = self.session.screen.query_one("#commsTL", expect_type=TextLog)
-        commsTL.write(f"{msg.message}")
+        if self.comms_textlog is None:
+            self.comms_textlog = self.session.screen.query_one("#commsTL", expect_type=TextLog)
+        self.comms_textlog.write(f"{msg.message}")
