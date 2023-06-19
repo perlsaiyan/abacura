35975b8e535b3aa7c1a22e785070b2a892a41c49
Tom Allen <tom@nonserviam.net>
2023-06-19 17:23:13 -0400

    finish BFS out with a visited list and add basic colorized terrains

--- abacura_kallisti/widgets/_lokmap.py
+++ abacura_kallisti/widgets/_lokmap.py
@@ -35,7 +35,6 @@ class LOKMap(Container):
 
 
     def compose(self) -> ComposeResult:
-        #with Middle():
         yield self.map
         if self.resizer:
             yield ResizeHandle(self, "bottom")
@@ -44,74 +43,76 @@ class LOKMap(Container):
         pass
 
     def generate_map(self) -> None:
+        #START_ROOM='3001'
         START_ROOM='3001'
         BFS = []
-
-        log("regenerate a MAP")
-        
+       
         H = self.content_size.height - 1
         W = self.content_size.width
         cH = int(H/3)
         cW = int(W/5)
-        fW = cW*5
-        #self.map.styles.width = fW
-        
-        
-
         cenH = int(cH/2)
         cenW = int(cW/2)
-        buf = f"width {self.map.container_size.width} or {W}\n"
-        buf += f"height {self.map.container_size.height} or {H}\n"
-        buf += f"viewport {self.content_size}\n"
-        buf += f"rooms: {cW}x{cH}\n"
-        buf += f"{cenW} {cenH}\n"
-        
 
-
-        # Test this
         # Y is first in the matrix!
         Matrix = [['' for x in range(cW)] for y in range(cH)]
         room = self.world.rooms[START_ROOM]
+        visited = {}
 
         BFS.append(MapPoint(room.vnum, cenW-1, cenH))
 
         #while we've got rooms, iterate them
         while len(BFS) > 0:
             here = BFS.pop(0)
-            log(f"Location: {here.room} @ {here.x} x {here.y} ({cW}x{cH})")
-            if here.room not in self.world.rooms:
+            if here.room == '' or not here.room in self.world.rooms:
                 continue
+
+            visited[here.room] = 1
             room = self.world.rooms[here.room]
             if Matrix[here.y][here.x] == '':
                 Matrix[here.y][here.x] = here.room
 
                 # Add exits to BFS
                 if (here.y-1) >= 0 and "north" in room.exits:
-                    log(f"appending {room.exits['north']}")
-                    BFS.append(MapPoint(room.exits["north"].to_vnum, here.x, here.y -1))
+                    if not room.exits["north"].to_vnum in visited:
+                        log(f"appending {room.exits['north']}")
+                        BFS.append(MapPoint(room.exits["north"].to_vnum, here.x, here.y -1))
 
                 if (here.y +1) < len(Matrix) and "south" in room.exits:
-                    log(f"appending {room.exits['south']}")
-                    BFS.append(MapPoint(room.exits["south"].to_vnum, here.x, here.y+1))
+                    if not room.exits["south"].to_vnum in visited:
+                        log(f"appending {room.exits['south']}")
+                        BFS.append(MapPoint(room.exits["south"].to_vnum, here.x, here.y+1))
 
                 if (here.x +1) < len(Matrix[here.y]) and "east" in room.exits:
-                    log(f"appending {room.exits['east']}")
-                    BFS.append(MapPoint(room.exits["east"].to_vnum, here.x+1, here.y))
+                    if not room.exits["east"].to_vnum in visited:
+                        log(f"appending {room.exits['east']}")
+                        BFS.append(MapPoint(room.exits["east"].to_vnum, here.x+1, here.y))
 
                 if (here.x -1) >= 0 and "west" in room.exits:
-                    log(f"appending {room.exits['west']}")
-                    BFS.append(MapPoint(room.exits["west"].to_vnum, here.x-1, here.y))
-
-        log(Matrix)
-        buf = f"{Matrix}"
-        
+                    if not room.exits["west"].to_vnum in visited:
+                        log(f"appending {room.exits['west']}")
+                        BFS.append(MapPoint(room.exits["west"].to_vnum, here.x-1, here.y))        
 
         # draw the map with Matrix of size Viewport
         buf =self.draw_map(Matrix, self.content_size.width, self.content_size.height-1, cur_room="3001")
         self.map.update(buf)
     
+    def get_terrain_icon(self, terrain: str) -> str:
+        if 'Inside' in terrain:
+            return "[on rgb(55,59,65)] [/on rgb(55,59,65)]"
+        if 'City' in terrain:
+            return "[on rgb(40,42,46)] [/on rgb(40,42,46)]"
+        if 'Path' in terrain:
+            return "[on rgb(92,49,20)] [/on rgb(92,49,20)]"
+        if 'Forest' in terrain:
+            return "[on rgb(28,93,25)] [/on rgb(28,93,25)]"
+        if 'Field' in terrain:
+            return "[on rgb(91,163,88)] [/on rgb(91,163,88)]"
+        if 'Water' in terrain:
+            return "[on rgb(52,124,186)] [/on rgb(52,124,186)]"
+        
+        return " "
     def draw_map(self, Matrix, cW, cH, cur_room: str="") -> str:
-        log("Draw Map")
         ytmp = len(Matrix) * 3
         xtmp = int((cW - len(Matrix[0])*5)/ 2)
         
@@ -128,37 +129,53 @@ class LOKMap(Container):
                     x += 5
                     continue
                     
-                log(f"drawing {self.world.rooms[xp].vnum} at {x} by {y}")
-
                 room = self.world.rooms[xp]
+                t_icon = self.get_terrain_icon(room.terrain)
                 if room.vnum == cur_room:
                     a_map[y][x] = "[bold red]@[/bold red]"
-                #a_map[y][x] = " "
+                
+                a_map[y-1][x-1] = t_icon
+                a_map[y-1][x-2] = t_icon
+                a_map[y-1][x+2] = t_icon
+
                 a_map[y][x-1] = "["
                 a_map[y][x+1] = "]"
+
+                a_map[y+1][x-1] = t_icon
+                a_map[y+1][x-2] = t_icon
+                a_map[y+1][x+2] = t_icon
+
                 if "north" in room.exits:
                     a_map[y-1][x] = "|"
+                else :
+                    a_map[y-1][x] = t_icon
                 if "south" in room.exits:
                     a_map[y+1][x] = "|"
+                else :
+                    a_map[y+1][x] = t_icon
                 if "east" in room.exits:
                     a_map[y][x+2] = "—"
+                else :
+                    a_map[y][x+2] = t_icon
                 if "west" in room.exits:
                     a_map[y][x-2] = "—"
+                else :
+                    a_map[y][x-2] = t_icon
                 if "up" in room.exits:
                     a_map[y-1][x+1] = "+"
+                else:
+                    a_map[y-1][x+1] = t_icon
                 if "down" in room.exits:
                     a_map[y+1][x+1] = "-"
-
+                else :
+                    a_map[y+1][x+1] = t_icon
                 x += 5
             y += 3
 
-        log(a_map)
         buf = ""
         for yp in a_map:
-            buf += ''.join(yp)
-            buf += "\n"
+            buf += ''.join(yp) + "\n"
  
-        log(buf)
         return buf
 
     def on_resize(self, event: Resize):
