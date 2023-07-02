952319dba5919119e6393cd7f9e985df914ba8ff
Tom Allen <tom@nonserviam.net>
2023-07-02 11:42:13 -0400

    Use renderable map instead of static widget, next we'll add toggles based on condition

--- abacura-kallisti/abacura_kallisti/widgets/_lokmap.py
+++ abacura-kallisti/abacura_kallisti/widgets/_lokmap.py
@@ -1,8 +1,14 @@
 """Kallisti Map Widget"""
 from __future__ import annotations
 
-import random
-from typing import TYPE_CHECKING, Optional
+from itertools import groupby
+from rich.color import Color, ColorType
+from rich.color_triplet import ColorTriplet
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.segment import Segment
+from rich.style import Style
+from rich.text import Text
+from typing import TYPE_CHECKING, Optional, Tuple
 
 from textual import log
 from textual.app import ComposeResult
@@ -18,6 +24,99 @@ from abacura.widgets.resizehandle import ResizeHandle
 if TYPE_CHECKING:
     from abacura.mud.session import Session
     from abacura_kallisti.atlas.world import World
+
+class LOKMapBlock:
+    terrains = {
+        'Inside': Color("Inside", ColorType(3), None, ColorTriplet(75,74,75)),
+        'Beach': Color("", ColorType(3), None, ColorTriplet(238,207,147)),
+        'City': Color("", ColorType(3), None, ColorTriplet(40,42,46)),
+        'Path': Color("", ColorType(3), None, ColorTriplet(92,49,20)),
+        'Forest': Color("", ColorType(3), None, ColorTriplet(28,93,25)),
+        'Jungle': Color("", ColorType(3), None, ColorTriplet(28,143,25)),
+        'Field': Color("", ColorType(3), None, ColorTriplet(91,163,88)),
+        'Hills': Color("", ColorType(3), None, ColorTriplet(121,163,88)),
+        'Desert': Color("", ColorType(3), None, ColorTriplet(243,203,147)),
+        'Shallow': Color("", ColorType(3), None, ColorTriplet(90,164,228)),
+        'Water': Color("", ColorType(3), None, ColorTriplet(52,124,186)),
+        'Deep': Color("", ColorType(3), None, ColorTriplet(52, 124,240)),
+        'Ocean': Color("", ColorType(3), None, ColorTriplet(52, 104,250)),
+        'Underground': Color("", ColorType(3), None, ColorTriplet(55,55,55)),
+        'Swamp': Color("", ColorType(3), None, ColorTriplet(55,85,85)),
+    }
+
+    def __init__(self, world: Optional[World], start_room, height, width):
+        self.start_room = start_room
+        self.world = world
+        self.width = width
+        self.height = height
+
+    def get_terrain2_icon(self, terrain: str) -> Color:
+        """Return terrain color/icon"""
+        return self.terrains.get(terrain.split()[0], Color("", ColorType(3), None, ColorTriplet(255,255,255)))
+
+    def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        if self.width == 0 or self.height == 0:
+            return
+        BFS = []
+
+        cenH = int(self.height/2)
+        cenW = int(self.width/2)
+
+        # Y is first in the matrix!
+        blank = Color("", ColorType(3), None, ColorTriplet(0,0,0))
+        Matrix = [[blank for x in range(self.width)] for y in range(self.height)]
+        try:
+            room = self.world.rooms[self.start_room]
+        except KeyError:
+            return
+        visited = {}
+
+        BFS.append(MapPoint(room.vnum, cenW, cenH))
+
+        while len(BFS) > 0:
+            here = BFS.pop(0)
+
+            if here.room == '' or not here.room in self.world.rooms:
+                continue
+
+            if here.room in visited:
+                continue
+            visited[here.room] = 1
+
+            room = self.world.rooms[here.room]
+            room_exits = room.exits  # get this once to improve wilderness performance
+                
+            if Matrix[here.y][here.x] == blank:
+                if room.vnum == self.start_room:
+                    Matrix[here.y][here.x] = Color("me",ColorType(3),None, ColorTriplet(255,255,255))
+                else:
+                    Matrix[here.y][here.x] = self.get_terrain2_icon(room.terrain_name)
+
+                # Add exits to BFS
+                if (here.y-1) >= 0 and "north" in room_exits:
+                    if not room_exits["north"].to_vnum in visited:
+                        #log(f"appending {room_exits['north']}")
+                        BFS.append(MapPoint(room_exits["north"].to_vnum, here.x, here.y -1))
+                if (here.y +1) < len(Matrix) and "south" in room_exits:
+                    if not room_exits["south"].to_vnum in visited:
+                        #log(f"appending {room_exits['south']}")
+                        BFS.append(MapPoint(room_exits["south"].to_vnum, here.x, here.y+1))
+                if (here.x +1) < len(Matrix[here.y]) and "east" in room_exits:
+                    if not room_exits["east"].to_vnum in visited:
+                        #log(f"appending {room_exits['east']}")
+                        BFS.append(MapPoint(room_exits["east"].to_vnum, here.x+1, here.y))
+                if (here.x -1) >= 0 and "west" in room_exits:
+                    if not room_exits["west"].to_vnum in visited:
+                        #log(f"appending {room_exits['west']}")
+                        BFS.append(MapPoint(room_exits["west"].to_vnum, here.x-1, here.y))        
+
+        for row in Matrix:
+            for color, n in [(color, len(list(g))) for color, g in groupby(row)]:
+                if color.name == "me":
+                    yield Segment("@", Style(color="red"))
+                else:
+                    yield Segment(" " * n, Style(bgcolor=color))
+            yield Segment("\n")
 
 class MapPoint():
     """Class to hold a room vnum and its location in our rooms matrix"""
@@ -47,6 +146,71 @@ class LOKMap(Container):
         self.screen.session.listener(self.recenter_map)
         self.world = self.screen.world
 
+    def generate_dense_map(self) -> None:
+        if self.START_ROOM == "":
+            log.warning("No start room present for map, do not draw")
+            return
+        BFS = []
+       
+        H = self.content_size.height - 1
+        W = self.content_size.width
+        cenH = int(H/2)
+        cenW = int(W/2)
+
+        # check if we're in a bad resize state and bail out
+        if H == 0 or W == 0:
+            return
+
+        # Y is first in the matrix!
+        Matrix = [[' ' for x in range(W)] for y in range(H)]
+        try:
+            room = self.world.rooms[self.START_ROOM]
+        except KeyError:
+            return
+        visited = {}
+
+        BFS.append(MapPoint(room.vnum, cenW-1, cenH))
+
+        #while we've got rooms, iterate them
+        while len(BFS) > 0:
+            here = BFS.pop(0)
+            if here.room == '' or not here.room in self.world.rooms:
+                continue
+
+            visited[here.room] = 1
+            room = self.world.rooms[here.room]
+            room_exits = room.exits  # get this once to improve wilderness performance
+            if Matrix[here.y][here.x] == ' ':
+                if room.vnum == self.START_ROOM:
+                    Matrix[here.y][here.x] = "@"
+                else:
+                    Matrix[here.y][here.x] = self.get_terrain2_icon(room.terrain_name)
+
+                # Add exits to BFS
+                if (here.y-1) >= 0 and "north" in room_exits:
+                    if not room_exits["north"].to_vnum in visited:
+                        log(f"appending {room_exits['north']}")
+                        BFS.append(MapPoint(room_exits["north"].to_vnum, here.x, here.y -1))
+
+                if (here.y +1) < len(Matrix) and "south" in room_exits:
+                    if not room_exits["south"].to_vnum in visited:
+                        log(f"appending {room_exits['south']}")
+                        BFS.append(MapPoint(room_exits["south"].to_vnum, here.x, here.y+1))
+
+                if (here.x +1) < len(Matrix[here.y]) and "east" in room_exits:
+                    if not room_exits["east"].to_vnum in visited:
+                        log(f"appending {room_exits['east']}")
+                        BFS.append(MapPoint(room_exits["east"].to_vnum, here.x+1, here.y))
+
+                if (here.x -1) >= 0 and "west" in room_exits:
+                    if not room_exits["west"].to_vnum in visited:
+                        log(f"appending {room_exits['west']}")
+                        BFS.append(MapPoint(room_exits["west"].to_vnum, here.x-1, here.y))        
+
+        # draw the map with Matrix
+        log(f"Matrix: {Matrix}")
+        buf = "\n".join([''.join(yp) for yp in Matrix])
+        self.map.update(Text.from_ansi(buf))
 
     def generate_map(self) -> None:
         #START_ROOM='3001'
@@ -114,6 +278,31 @@ class LOKMap(Container):
         buf = self.draw_map(Matrix, self.content_size.width, self.content_size.height-1)
         self.map.update(buf)
 
+    def get_terrain2_icon(self, terrain: str) -> str:
+        """Return terrain color/icon"""
+        log(f"TERRAIN: {terrain}")
+        if 'Inside' in terrain:
+            return "\x1b[48;2;55;59;65m \x1b[0m"
+        if 'Beach' in terrain:
+            return "\x1b[48;2;238;207;147m \x1b[0m"
+        if 'City' in terrain:
+            return "\x1b[48;2;40;42;46m \x1b[0m"
+        if 'Path' in terrain:
+            return "\x1b[48;2;92;49;20m \x1b[0m"
+        if 'Forest' in terrain:
+            return "\x1b[48;2;28;93;25m \x1b[0m"
+        if 'Field' in terrain:
+            return "\x1b[48;2;91;163;88m \x1b[0m"
+        if 'Desert' in terrain:
+            return "\x1b[48;2;243;203;147m \x1b[0m"
+        if 'Shallow Water' in terrain:
+            return "\x1b[48;2;90;164;228m \x1b[0m"
+        if 'Water' in terrain:
+            return "\x1b[48;2;52;124;186m \x1b[0m"
+
+        return " "
+
+
     def get_terrain_icon(self, terrain: str) -> str:
         """Return terrain color/icon"""
         if 'Inside' in terrain:
@@ -136,7 +325,7 @@ class LOKMap(Container):
             return "[on rgb(52,124,186)] [/on rgb(52,124,186)]"
         
         return " "
-
+        
     def draw_map(self, Matrix, cW, cH) -> str:
         xtmp = int((cW - len(Matrix[0])*5)/ 2)
         
@@ -197,10 +386,15 @@ class LOKMap(Container):
         return "\n".join([''.join(yp) for yp in a_map])
 
     def on_resize(self, event: Resize):
-        self.generate_map()
+        #self.generate_map()
+        self.map.update(LOKMapBlock(self.world, self.START_ROOM, self.content_size.height, self.content_size.width))
 
     @event("msdp_value_ROOM_VNUM")
     def recenter_map(self, message: MSDPMessage):
         """Event to trigger map redraws on movement"""
         self.START_ROOM = str(message.value)
-        self.generate_map()
+        #self.generate_dense_map()
+
+        LMB = LOKMapBlock(self.world, self.START_ROOM, self.content_size.height, self.content_size.width)
+        self.map.update(LMB)
+        
