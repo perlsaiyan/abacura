2b4833df09a58155a3a9b9e1c84802c08205c46b
Mike Heeter <heeterlab@fastmail.fm>
2023-07-03 14:12:06 -0500

    Improve python profiler to double check cprofiler

--- abacura-core/abacura/utils/profiler.py
+++ abacura-core/abacura/utils/profiler.py
@@ -1,43 +1,113 @@
 from time import time
+from dataclasses import dataclass
 import threading
 import sys
 from collections import deque
+from typing import Dict
+
 try:
     from resource import getrusage, RUSAGE_SELF
 except ImportError:
     RUSAGE_SELF = 0
-    def getrusage(who=0):
-        return [0.0, 0.0] # on non-UNIX platforms cpu_time always 0.0
 
-p_stats = None
+
+    def getrusage(_who=0):
+        return [0.0, 0.0]  # on non-UNIX platforms cpu_time always 0.0
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Function:
+    co_name: str = ''
+    co_filename: str = ''
+    co_first_lineno: int = 0
+
+    def get_location(self) -> str:
+        return f"{self.co_filename}:{self.co_name}[{self.co_first_lineno}]"
+
+
+@dataclass(slots=True)
+class FunctionStats:
+    function: Function
+    call_count: int = 0
+    elapsed_time: float = 0
+    cpu_time: float = 0
+    child_time: float = 0
+    child_cpu_time: float = 0
+
+    @property
+    def self_time(self) -> float:
+        return self.elapsed_time - self.child_time
+
+    @property
+    def self_cpu_time(self) -> float:
+        return self.cpu_time - self.child_cpu_time
+
+
+@dataclass(slots=True)
+class FunctionCall:
+    function: Function = Function()
+    start_time: float = 0
+    start_cpu_time: float = 0
+    child_time: float = 0
+    child_cpu_time: float = 0
+
+
+p_stats: Dict[Function, FunctionStats] = {}
 p_start_time = None
 p_profiling = False
 
-def profiler(frame, event, arg):
-    if event not in ('call','return'): return profiler
-    #### gather stats ####
+
+def profiler(frame, event, _arg):
+    global p_stats
+    if event not in ('call', 'return'):
+        return profiler
+
+    # gather stats
     rusage = getrusage(RUSAGE_SELF)
-    t_cpu = rusage[0] + rusage[1] # user time + system time
+    t_cpu = rusage[0] + rusage[1]  # user time + system time
     code = frame.f_code
-    fun = (code.co_name, code.co_filename, code.co_firstlineno)
-    #### get stack with functions entry stats ####
+    function = Function(code.co_name, code.co_filename, code.co_firstlineno)
+
+    # get stack with functions entry stats
     ct = threading.current_thread()
     try:
         p_stack = ct.p_stack
     except AttributeError:
         ct.p_stack = deque()
         p_stack = ct.p_stack
-    #### handle call and return ####
+
+    # handle call and return #
     if event == 'call':
-        p_stack.append((time(), t_cpu, fun))
-    elif event == 'return':
-        try:
-            t,t_cpu_prev,f = p_stack.pop()
-            assert f == fun
-        except IndexError: # TODO investigate
-            t,t_cpu_prev,f = p_start_time, 0.0, None
-        call_cnt, t_sum, t_cpu_sum = p_stats.get(fun, (0, 0.0, 0.0))
-        p_stats[fun] = (call_cnt+1, t_sum+time()-t, t_cpu_sum+t_cpu-t_cpu_prev)
+        p_stack.append(FunctionCall(function, time(), t_cpu))
+        return profiler
+
+    # return
+    try:
+        function_call: FunctionCall = p_stack.pop()
+        assert function_call.function == function
+    except IndexError:
+        # TODO investigate
+        return profiler
+
+    if function in p_stats:
+        function_stats = p_stats[function]
+    else:
+        function_stats = FunctionStats(function)
+        p_stats[function] = function_stats
+
+    call_time = time() - function_call.start_time
+    cpu_time = t_cpu - function_call.start_cpu_time
+    function_stats.call_count += 1
+    function_stats.elapsed_time += call_time
+    function_stats.cpu_time += cpu_time
+    function_stats.child_time += function_call.child_time
+    function_stats.child_cpu_time += function_call.child_cpu_time
+
+    if len(p_stack):
+        parent: FunctionCall = p_stack[-1]
+        parent.child_time += call_time
+        parent.child_cpu_time += cpu_time
+
     return profiler
 
 
@@ -49,6 +119,7 @@ def profile_on():
     sys.setprofile(profiler)
     p_profiling = True
 
+
 def profile_off():
     global p_profiling
     threading.setprofile(None)
@@ -56,11 +127,8 @@ def profile_off():
     p_profiling = False
 
 
-def get_profile_stats():
+def get_profile_stats() -> Dict[Function, FunctionStats]:
     """
-    returns dict[function_tuple] -> stats_tuple
-    where
-      function_tuple = (function_name, filename, lineno)
-      stats_tuple = (call_cnt, real_time, cpu_time)
+    returns dict[Function] -> FunctionStats
     """
     return p_stats
