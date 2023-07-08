98cfd2006836bbfbbd3543f70744917d0069ab56
Tom Allen <tom@nonserviam.net>
2023-07-08 19:54:52 -0400

    add ansi_escape to abacura.utils

--- abacura-core/abacura/utils/__init__.py
+++ abacura-core/abacura/utils/__init__.py
@@ -1,5 +1,6 @@
 from numbers import Real
 from collections import OrderedDict
+import re
 
 _pct_colors = OrderedDict()
 _pct_colors[80] = "green"
@@ -7,6 +8,8 @@ _pct_colors[60] = "green_yellow"
 _pct_colors[40] = "yellow"
 _pct_colors[20] = "dark_orange3"
 _pct_colors[0]  = "red"
+
+ansi_escape = re.compile(r'\x1b(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
 def percent_color(cval: Real) -> str:
     for key, value in _pct_colors.items():
