2648405918c60712e0f20d88b68cfdb88e5838d3
Tom Allen <tom@nonserviam.net>
2023-07-26 08:37:38 -0400

    Remove action widget for now

--- abacura-kallisti/abacura_kallisti/widgets/_lokright.py
+++ abacura-kallisti/abacura_kallisti/widgets/_lokright.py
@@ -1,13 +1,11 @@
 """Legends of Kallisti Right Side Panel Dock"""
-from textual import log
 from textual.app import ComposeResult
 from textual.containers import Container
-from textual.widgets import Static
+
+from abacura_kallisti.widgets import LOKMap, LOKZone, LOKGroup, LOKCombat
 
 from abacura.widgets.sidebar import Sidebar
 from abacura.widgets.resizehandle import ResizeHandle
-
-from abacura_kallisti.widgets import LOKMap, LOKZone, LOKGroup, LOKCombat
 
 class LOKRight(Sidebar):
     """Right hand dock, intended for user widgets"""
@@ -18,6 +16,3 @@ class LOKRight(Sidebar):
             yield LOKZone(id="lokzone")
             yield LOKGroup(id="lokgroup")
             yield LOKCombat(id="lokcombat")
-            yield Static("Action Queues", classes="WidgetTitle")
-            yield Static("[green]Priority: [white]0 [green]Heal: [white]0  [green]Any: [white]0\n" + \
-                         "[green]  Combat: [white]0 [green] NCO: [white]0 [green]Move: [white]0")
