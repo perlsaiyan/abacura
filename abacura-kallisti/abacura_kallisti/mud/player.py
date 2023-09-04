45a20772e2f3e711e4030c7a6b6a42ccbe5ff522
Tom Allen <tom@nonserviam.net>
2023-09-04 16:50:31 -0400

    Adds pc.save() and support for harvesting subsection, will move to more generic code later

--- abacura-kallisti/abacura_kallisti/mud/player.py
+++ abacura-kallisti/abacura_kallisti/mud/player.py
@@ -3,9 +3,9 @@ Player structure
 
 Includes current player information, and preferences
 """
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields
 import os
-from tomlkit import parse, TOMLDocument, document
+from tomlkit import parse, TOMLDocument, document, table
 from typing import Callable
 from abacura import Config
 
@@ -34,6 +34,18 @@ class PlayerSkill:
     rank_bonus: int = 0
     locked: bool = False
 
+@dataclass
+class PlayerHarvesting:
+    """Toggles for harvesting skills"""
+    butchering: bool = False
+    chirurgy: bool = False
+    fishing: bool = False
+    gathering: bool = False
+    logging: bool = False
+    poisoncraft: bool = False
+    skinning: bool = False
+    
+
 
 @dataclass(slots=True)
 class PlayerCharacter:
@@ -43,10 +55,22 @@ class PlayerCharacter:
     egress_vnum: str = '3001'
     recall_vnum: str = '3001'
     char_name: str = ''
+    char_file: str = ''
     skills: dict[str, PlayerSkill] = field(default_factory=dict)
     buffs: list[str] = field(default_factory=list)
     # consumables: list[Consumable] = field(default_factory=list)
     target_heros: int = 0
+    logging: bool = False
+
+    harvesting: PlayerHarvesting = field(default_factory=PlayerHarvesting)
+
+    def save(self):
+        # convert our convenience stuff back into the _config
+        self.save_harvesting()
+
+        # write to disk
+        with open(self.char_file, 'w', encoding="UTF-8") as fp:
+            fp.write(self._config.as_string())
 
     def load(self, data_dir: str, name: str):
         """Load a player character record from disk"""
@@ -54,7 +78,8 @@ class PlayerCharacter:
 
         char_file = os.path.join(data_dir, self.char_name)
         char_file = f"{char_file}.player"
-        if not os.path.isfile(char_file):
+        self.char_file = char_file
+        if not os.path.isfile(self.char_file):
             with open(char_file, 'w', encoding="UTF-8") as fp:
                 fp.write(f"char_name = '{self.char_name}'\n")
 
@@ -62,3 +87,20 @@ class PlayerCharacter:
         for key, val in self._config.items():
             if hasattr(self, key):
                 setattr(self, key, val)
+
+        # populating harvesting record
+        self.parse_harvesting()
+
+    def parse_harvesting(self):
+        """Pull harvesting settings out of config"""
+        harvest_conf = self._config.get("harvesting", {})
+        for skill in fields(PlayerHarvesting):
+            setattr(self.harvesting, skill.name, harvest_conf.get(skill.name, False))
+
+    def save_harvesting(self):
+        """Put harvesting settings into config"""
+        harvesting = table()
+        for skill in fields(PlayerHarvesting):
+            harvesting[skill.name] = getattr(self.harvesting, skill.name, False)
+
+        self._config["harvesting"] = harvesting
