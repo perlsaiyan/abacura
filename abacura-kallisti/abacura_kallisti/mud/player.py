05f250ff5901b79e5824da55c4884d51ec57a1f1
Tom Allen <tom@nonserviam.net>
2023-10-14 20:38:36 -0400

    Player record updates and telluria zone additions

--- abacura-kallisti/abacura_kallisti/mud/player.py
+++ abacura-kallisti/abacura_kallisti/mud/player.py
@@ -5,10 +5,8 @@ Includes current player information, and preferences
 """
 from dataclasses import dataclass, field, fields
 import os
+from typing import Dict
 from tomlkit import parse, TOMLDocument, document, table
-from typing import Callable
-from abacura import Config
-
 
 # class Consumable:
 #     short_name: str = ''
@@ -25,7 +23,7 @@ from abacura import Config
 
 @dataclass(slots=True)
 class PlayerSkill:
-    skill: str = ''
+    skill: str = ""
     clevel: int = 0
     mp_sp: int = 0
     skilled: int = 0
@@ -34,9 +32,11 @@ class PlayerSkill:
     rank_bonus: int = 0
     locked: bool = False
 
+
 @dataclass
 class PlayerHarvesting:
     """Toggles for harvesting skills"""
+
     butchering: bool = False
     chirurgy: bool = False
     fishing: bool = False
@@ -44,31 +44,35 @@ class PlayerHarvesting:
     logging: bool = False
     poisoncraft: bool = False
     skinning: bool = False
-    
 
 
 @dataclass(slots=True)
 class PlayerCharacter:
     """Kallisti specific player information"""
+
     _config: TOMLDocument = field(default_factory=document)
-    home_vnum: str = ''
-    egress_vnum: str = '3001'
-    recall_vnum: str = '3001'
-    char_name: str = ''
-    char_file: str = ''
+    home_vnum: str = ""
+    egress_vnum: str = "3001"
+    recall_vnum: str = "3001"
+    char_name: str = ""
+    char_file: str = ""
     skills: dict[str, PlayerSkill] = field(default_factory=dict)
     buffs: list[str] = field(default_factory=list)
     # consumables: list[Consumable] = field(default_factory=list)
     target_heros: int = 0
 
     harvesting: PlayerHarvesting = field(default_factory=PlayerHarvesting)
+    meta_gold_cost: Dict[str, int] = field(default_factory=dict)
+    meta_xp_cost: Dict[str, int] = field(default_factory=dict)
+    meta_exp_per_hero: Dict[str, float] = field(default_factory=dict)
 
     def save(self):
         # convert our convenience stuff back into the _config
         self.save_harvesting()
+        self.save_meta()
 
         # write to disk
-        with open(self.char_file, 'w', encoding="UTF-8") as fp:
+        with open(self.char_file, "w", encoding="UTF-8") as fp:
             fp.write(self._config.as_string())
 
     def load(self, data_dir: str, name: str):
@@ -79,7 +83,7 @@ class PlayerCharacter:
         char_file = f"{char_file}.player"
         self.char_file = char_file
         if not os.path.isfile(self.char_file):
-            with open(char_file, 'w', encoding="UTF-8") as fp:
+            with open(char_file, "w", encoding="UTF-8") as fp:
                 fp.write(f"char_name = '{self.char_name}'\n")
 
         self._config = parse(open(char_file, "r", encoding="UTF-8").read())
@@ -104,3 +108,7 @@ class PlayerCharacter:
 
         self._config["harvesting"] = harvesting
 
+    def save_meta(self):
+        self._config["meta_gold_cost"] = self.meta_gold_cost
+        self._config["meta_xp_cost"] = self.meta_xp_cost
+        self._config["meta_exp_per_hero"] = self.meta_exp_per_hero
